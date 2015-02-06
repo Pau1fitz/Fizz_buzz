c8e6c7b7a11a988e941e9f531385defd4538e28f
Paul Fitzgerald <paulfitz99@gmail.com>
2015-02-06 08:44:39 +0000

    second commit

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,42 +1,45 @@
-require 'fizzbuzz.rb'
+require 'fizzbuzz.rb' 
 
-describe 'FizzBuzz' do 
-  let(:fizzbuzz) {FizzBuzz.new}
-  it "knows if a number is divisible by 3" do
-    expect(fizzbuzz.is_divisible_by_three?(1)).to eq false 
+describe Fizzbuzz do 
+
+  let(:fizzbuzz){Fizzbuzz.new}
+  
+  it 'knows that 1 is not divisible by 3' do
+    expect(fizzbuzz.divided_by_three?(1)).to eq false
   end
 
-  it "knows 3 is divisible by 3" do
-    expect(fizzbuzz.is_divisible_by_three?(3)).to eq true
+  it 'knows that 3 is divisible by 3' do
+    expect(fizzbuzz.divided_by_three?(3)).to eq true
   end
 
-  it "knows if a number is divisible by 5" do
-    expect(fizzbuzz.is_divisible_by_five?(1)).to eq false
+  it 'knows that 1 is not divisible by 5' do
+    expect(fizzbuzz.divided_by_five?(1)).to eq false
+  end  
+
+  it 'knows that 5 is divisible by 5' do
+    expect(fizzbuzz.divided_by_five?(5)).to eq true
   end
 
-  it "knows that 5 is divisible by 5" do
-    expect(fizzbuzz.is_divisible_by_five?(5)).to eq true
-  end
-  it "knows that a number is divisible by 15" do
-    expect(fizzbuzz.is_divisible_by_fifteen?(1)).to eq false
-  end
-  it "knows that 15 is divisible" do
-    expect(fizzbuzz.is_divisible_by_fifteen?(15)).to eq true
+  it 'knows that 1 is not divisible by 15' do
+    expect(fizzbuzz.divided_by_fifteen?(1)).to eq false
   end
 
-  it "returns 1 for 1 when playing" do
-    expect(fizzbuzz.play(1)).to eq 1
+  it 'knows that 15 is divisible by 15' do
+    expect(fizzbuzz.divided_by_fifteen?(15)).to eq true
   end
 
-    it "returns fizz for 3 when playing" do
-      expect(fizzbuzz.play(3)).to eq "fizz"
+  it 'knows when player enters number one to return one' do
+    expect(fizzbuzz.play(1)).to eq (1)
   end
 
-    it "returns buzz for 5 when playing" do
-      expect(fizzbuzz.play(5)).to eq "buzz"
-    end
+  it 'knows when a player enters number three to return fizz' do
+    expect(fizzbuzz.play(3)).to eq "fizz"
+  end
 
-    it "returns fizzbuzz for 15 when playing" do
-      expect(fizzbuzz.play(15)).to eq "fizzbuzz"
-    end
+  it 'knows to return buzz when number is divisible by 5' do
+    expect(fizzbuzz.play(5)).to eq "buzz"
+  end
+  it 'knows to return fizzbuzz if number divisible by 15' do
+    expect(fizzbuzz.play(15)).to eq "fizzbuzz"
+  end
 end
